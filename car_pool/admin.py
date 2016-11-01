cbcb9ae907918f376f97ccabfc9beb4681da17fb
08_graham@live.co.uk <08_graham@live.co.uk>
2016-11-01 14:17:54 +0000

    changes to models in line with class diagram " ============================================================================ " Netrw Directory Listing                                        (netrw v140) "   /Users/Graham/Desktop/testing "   Sorted by      name "   Sort sequence: [\/]$,\<core\%(\.\d\+\)\=\>,\.h$,\.c$,\.cpp$,*,\.o$,\.obj$,\.info$,\.swp$,\.bak$,\~$ "   Quick Help: <F1>:help  -:go up dir  D:delete  R:rename  s:sort-by  x:exec " ============================================================================ ../ .git/ blog/ mysite/ .DS_Store .gitignore db.sqlite3 manage.py*

--- car_pool/admin.py
+++ car_pool/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Car, Staff
+from .models import Car, Staff, Faculty, School, Journey
 
 admin.site.register(Car)
 admin.site.register(Staff)
+admin.site.register(Faculty)
+admin.site.register(School)
+admin.site.register(Journey)
--- car_pool/models.py
+++ car_pool/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 class Staff(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    school = models.TextField()
+    schoolOfWork = models.TextField()
     driver = models.BooleanField()
     headOfSchool = models.BooleanField()
     head0fFaculty = models.BooleanField()
@@ -16,8 +16,39 @@ class Car(models.Model):
     regNum = models.TextField()
     model = models.TextField()
     colour = models.TextField()
-    driver = models.ForeignKey('auth.User')
+    driver = models.ForeignKey('Staff')
+    numberOfSeats = models.IntegerField(default='0')
+
 
     def __str__(self):
         description = self.model + " " + self.colour
         return description
+
+class Faculty(models.Model):
+    facultyName = models.TextField()
+    headOfFaculty = models.ForeignKey('Staff')
+
+    def __str__(self):
+        return self.facultyName
+
+class School(models.Model):
+    schoolName = models.TextField()
+    faculty = models.ForeignKey('Faculty')
+    headOfSchool = models.ForeignKey('Staff')
+
+    def __str__(self):
+        return self.schoolName
+
+
+class Journey(models.Model):
+    driver = models.ForeignKey('Staff')
+    car = models.ForeignKey('Car')
+    origin = models.TextField() # add chioces parameter
+    destination = models.TextField() # add choices paramter
+    dateOfJourney = models.DateField()
+    departureTime = models.TimeField()
+    arrivalTime = models.TimeField()
+
+    def __str__(self):
+        journey = self.destination + " to " + self.destination + " on " + self.dateOfJourney
+        return journey
